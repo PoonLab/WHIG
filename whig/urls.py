d4489e54ccbf0ebce340f83cd650721210590d0d
0ldm4j0r <0ldm4j0r@gmail.com>
2017-05-30 10:34:17 -0400

    Created models for the portal app.

--- whig/urls.py
+++ whig/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^investigators/', include('InvestigatorsAndProfiles.urls')),
-    url(r'^meetings/', include('WHIGMeetings.urls')),
-    url(r'^news/', include('WHIGNews.urls')),
+    url(r'^portal/', include('portal.urls')),
+    url(r'^members/', include('members.urls')),
+    url(r'^meetings/', include('meetings.urls')),
+    url(r'^news/', include('news.urls')),
+    url(r'^publications/', include('publications.urls')),
 ]
