99f811fc573c812755672e944ae11e1102ed05c8
0ldm4j0r <0ldm4j0r@gmail.com>
2017-05-30 10:58:27 -0400

    Created models for the members app.

--- members/models.py
+++ members/models.py
@@ -1,4 +1,25 @@
 from django.db import models
 
+class Member(models.Model):
+    given_name = models.CharField(max_length = 1000)
+    surname = models.CharField(max_length = 1000)
+    profile = models.CharField(max_length = 1000000)
+    website = models.CharField(max_length = 1000)
+    ORCID = models.CharField(max_length = 1000)
+    is_active = models.BooleanField()
+    is_core = models.BooleanField()
+    # Pictures are to be uploaded via the administrator interface as files.
 
+class Faculty(models.Model):
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
+    rank = models.CharField(max_length = 1000) # Position
 
+class Trainee(models.Model):
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
+    rank = models.CharField(max_length = 1000) # Position
+    PI = models.CharField(max_length = 1000) # Principal Investigator / Adviser
+
+class Staff(models.Model):
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
+    rank = models.CharField(max_length = 1000) # Position
+    PI = models.CharField(max_length = 1000) # Principal Investigator / Adviser
