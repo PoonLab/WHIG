4d25e912394132b5d5fbe32857e1fdef02f0b50f
0ldm4j0r <0ldm4j0r@gmail.com>
2017-05-30 11:40:32 -0400

    Created models for the meetings app.

--- meetings/models.py
+++ meetings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from members.models import Faculty, Trainee, Staff
+
+class Publication(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete = models.CASCADE)
+    trainee = models.ForeignKey(Trainee, on_delete = models.CASCADE)
+    staff = models.ForeignKey(Staff, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 1000)
+    date = models.DateField(auto_now = False, auto_now_add = False)
+    authors_line = models.CharField(max_length = 1000)
 
 
