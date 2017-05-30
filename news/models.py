ae648e60f634aeb3efd7176d2fbc47a2d58e03ac
0ldm4j0r <0ldm4j0r@gmail.com>
2017-05-30 11:27:20 -0400

    Created models for the news app.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-
+class Article(models.Model):
+    title = models.CharField(max_length = 1000)
+    body = models.CharField(max_length = 1000000)
+    date = models.DateField.auto_now_add
+    lastModified = models.DateField.auto_now
+    URL =  models.CharField(max_length = 1000)
+    # Pictures are to be uploaded via the administrator interface as files.
