f0f2142a67bcedcd2535d297e52b5ba6e8c61f8f
0ldm4j0r <0ldm4j0r@gmail.com>
2017-05-30 09:18:06 -0400

    Initialized 5 new apps (portal, members, meetings, news, publications).

--- whig/settings.py
+++ whig/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'InvestigatorsAndProfiles.apps.InvestigatorsAndProfilesConfig',
-    'WHIGMeetings.apps.WHIGMeetingsConfig',
-    'WHIGNews.apps.WHIGNewsConfig',
+    #'InvestigatorsAndProfiles.apps.InvestigatorsAndProfilesConfig',
+    #'WHIGMeetings.apps.WHIGMeetingsConfig',
+    #'WHIGNews.apps.WHIGNewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
